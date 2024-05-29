f5f351a99276aeca040761a0fa512b357e1b5a31
romario lima <romariolima1006@gmail.com>
2024-05-29 13:19:59 -0300

    serializers CategorySerializer e ContactSerializer

---
+++ contact/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Category, Contact
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    '''serializador da do model Category'''
+
+    class Meta:
+        model = Category
+        fields = ['id', 'url' 'name', ]
+
+
+class ContactSerializer(serializers.HyperlinkedModelSerializer):
+    '''serializador do model Contact'''
+    class Meta:
+        model = Category
+        fields = [
+            'id', 'url', 'first_name', 'last_name', 'phone', 'email', 'description', 'show', 'picture', 'category'
+        ]
